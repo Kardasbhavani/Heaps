e74f60e276f6a5043e4cf7593bc78379b6a65e4d
Kardasbhavani <bhavanikardas@gmail.com>
2026-04-25 21:55:01 +0530

    Add code360 comment at the beginning of Build heap.py

---
+++ Build heap.py
@@ -0,0 +1,16 @@
+#code360
+def buildHeap(arr, n):
+    def heapifydown(arr,index):
+        largest=index
+        lchild=2*index+1
+        rchild=2*index+2
+        if(lchild<n and arr[lchild]>arr[largest]):
+            largest=lchild
+        if(rchild<n and arr[rchild]>arr[largest]):
+            largest=rchild
+        if(index!=largest):
+            arr[largest],arr[index]=arr[index],arr[largest]
+            heapifydown(arr,largest)
+    for index in range(n//2-1,-1,-1):
+        heapifydown(arr,index)
+    return arr
